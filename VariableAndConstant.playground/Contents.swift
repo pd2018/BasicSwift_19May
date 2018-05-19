2d9a6bc99e4fd7bd3ec4f6d0010a9d48a98436f4
dui <iam.narongsak@gmail.com>
2018-05-19 10:30:36 +0700

    Variable And Constant

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร ที่สามารถแก้ไขค่าได้
+//Implicit คือการประกาศตัวแปร ที่ใช้ Value กำหนด Type Data
+var nameString = "Master UNG"       // String Type
+var numberInt = 123     // Int Type
+var numberDou = 3.1416  // Double Type
+var statusBol = true
+
+nameString = "Nobita"
+numberInt = 555
+
+
+//การกำหนดค่าคงที่
+let surnameString = "Theppitak"
+
+//surnameString = "NewTheppitak" ไม่สามารถ Compile ได้
+
+let numInt = 100
+// numInt = 200
+
+//Explicit คือการประกาศตัวแปร แบบ กำหนด Data Type ได้
+var name2String: String = "Dui"
+var num2Int: Int = 123
+
+
+
+
