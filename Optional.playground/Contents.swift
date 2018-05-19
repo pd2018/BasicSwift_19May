801cafeab229959210237b7f24ecbbb629405cee
dui <iam.narongsak@gmail.com>
2018-05-19 13:53:57 +0700

    Learn Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numberInt: Int?
+
+
+
+var numberString = "9"
+
+print("numberInt ==> \(numberInt)")
+
+var number2Int = Int(numberString)
+
+var answerInt = number2Int! * 5
+
+
+print("Answer ==> \(answerInt)")
+
