bded474ee816564944d1e2be883f4f227fc53d42
dui <iam.narongsak@gmail.com>
2018-05-20 09:58:51 +0700

    Update

--- ClassAndFaction.playground/Contents.swift
+++ ClassAndFaction.playground/Contents.swift
@@ -12,7 +12,29 @@ class Myclass {
         print("numberInt * 2 ==> \(numberInt)")
     }
     
+    func myFunctionVoidParameters(increseInt: Int) -> Void {
+        let resultInt = numberInt + increseInt
+        print("Result ==> \(resultInt)")
+        
+        
+    }
+    func myFunctionReturnType() -> String {
+        
+        let resultString = nameString + " Make In Japan"
+        
+        return resultString
+        
+    }
     
+    func myFunctionReturnParameters(baseInt: Int, heightInt: Int, unit: String) -> String {
+        
+        let areaDou: Double = 0.5 * Double(baseInt) * Double(heightInt)
+        
+        let resultString = String(areaDou) + " Sq." + unit
+        
+        return resultString
+        
+    }
     
     
 }   // MyClass
@@ -31,12 +53,21 @@ objMyClass.nameString = "itrex"
 //หลังแก้ไข
 print("Name After ==> \(objMyClass.nameString)")
 
+//Demo Call Function Void Type by object
 objMyClass.myFunctionVoidType()
 objMyClass.myFunctionVoidType()
 
+//Demo Call Function Void and Add Parameter
+objMyClass.myFunctionVoidParameters(increseInt: 98)
 
+//Demo Call Function Reture Type
+var myResult = objMyClass.myFunctionReturnType()
+print("Value of myResult ==> \(myResult)")
 
+//Demo Call Function Return Type Add Parameter
+var areaResult = objMyClass.myFunctionReturnParameters(baseInt: 14, heightInt: 5, unit: "Meter")
 
+print("Area ==> \(areaResult)")
 
 
 
